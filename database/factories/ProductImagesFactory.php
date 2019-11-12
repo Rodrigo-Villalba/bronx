550768c244d6f5d7e46b27c39f0b9dcac5a799de
rodrigo <rodrigogastonvillalba@gmail.com>
2019-11-12 11:03:37 -0300

    creada carpeta productos image

---
+++ database/factories/ProductImagesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\productImages;
+use Faker\Generator as Faker;
+
+$factory->define(productImages::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
