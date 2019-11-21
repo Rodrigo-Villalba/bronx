41f08d0edd99f1207b7f571b18d39384d2a180f5
DylanNoso <dylannoso@gmail.com>
2019-11-21 12:33:09 -0300

    empezando con as vistas

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/product/{product}', 'ProductController@show');
+// Route::get('/register', 'RegistrerController@show');
