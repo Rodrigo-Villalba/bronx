93f9c7455dd5a04891f7594927094e5ba3f16201
rodrigo <rodrigogastonvillalba@gmail.com>
2019-11-30 03:58:06 -0300

    paso en limpio el carrito

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,205 +4,39 @@
 <div class="container">
   <h2>Panel Admin</h2>
 
-  <p>hola administradores</p>
+  <button class="btn btn-success" onclick="window.location='{{ url("formularioCarga") }}'">Agregar producto</button>
 
- <a href="/formularioCarga" class = "dropdown-quantity-item selected" name="button">Agregar Producto</a><br>
+  <br />
+  <br />
 
-<br>
-  <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">name</th>
-      <th scope="col">descriptions</th>
-      <th scope="col">price</th>
-      <th scope="col">featured_img</th>
-      <th scope="col">stock</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Sillon</td>
-      <td>sillon</td>
-      <td>$5000</td>
-      <td>
-        <img src="\storage\products\sillonfreddy\sillonfreddy.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-      <th scope="row">2</th>
-      <td>Mesa</td>
-      <td>mesa hierro</td>
-      <td>$3000</td>
-      <td>
-      <img src="\storage\products\recibidoreva\eva.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Mesita Rocks</td>
-      <td>mesa ratona rocks</td>
-      <td>$2000</td>
-      <td>
-      <img src="\storage\products\mesarock\mesarock.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>Barra pill</td>
-      <td>barra de comedor</td>
-      <td>$5000</td>
-      <td>
-      <img src="\storage\products\barrapill\barrapill.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">5</th>
-      <td>Espejo triangle</td>
-      <td>espejo de hierro</td>
-      <td>$4500</td>
-      <td>
-      <img src="\storage\products\espejotriangle\espejotriangle.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">6</th>
-      <td>Estanteria oval</td>
-      <td>estante en forma redonda</td>
-      <td>$6000</td>
-      <td>
-      <img src="\storage\products\estanteriaoval\estanteriaoval.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">7</th>
-      <td>Llavero mesh</td>
-      <td>recibidor de hierro para llaves</td>
-      <td>$3500</td>
-      <td>
-      <img src="\storage\products\llaveromesh\llaveromesh.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">8</th>
-      <td>Mueble savanna</td>
-      <td>mueble de tv</td>
-      <td>$5400</td>
-      <td>
-      <img src="\storage\products\mueblesavanna\mueblesavanna.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">9</th>
-      <td>Perchero lenny</td>
-      <td>perchero de hierro de pie</td>
-      <td>$2500</td>
-      <td>
-      <img src="\storage\products\percherolenny\percherolenny.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">10</th>
-      <td>Zapatero torre</td>
-      <td>zapatero de hierro</td>
-      <td>$7600</td>
-      <td>
-      <img src="\storage\products\zapaterotorre\zapaterotorre.jpg" width="50">
-      </td>
-      <td>
-        <input name="qty" id="qty" maxlength="12" value="1" title="Cantidad" class="input-text qty" type="text">
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Editar</button>
-      </td>
-      <td>
-       <button class = "dropdown-quantity-item selected" name="button">Borrar</button>
-      </td>
-    </tr>
-    
-  </tbody>
-</table>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Descripcion</th>
+        <th scope="col">Categoria</th>
+        <th scope="col">Precio</th>
+        <th scope="col">Stock</th>
+        <th scope="col">#</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($products as $product)
+        <tr>
+          <td>{{$product->id}}</td>
+          <td>{{$product->name}}</td>
+          <td>{{$product->description}}</td>
+          <td>{{$product->categoria->nombre}}</td>
+          <td>{{$product->price}}</td>
+          <td>{{$product->stock}}</td>
+          <td>
+              <a href="{{ url('/productos/' . $product->id) }}" class="btn btn-xs btn-info pull-right">Ver producto</a>
+          </td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
 
 </div>
 @endsection
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -10,25 +10,41 @@
           <th scope="col">Producto</th>
           <th scope="col">Cantidad</th>
           <th scope="col">Precio</th>
+          <th scope="col">SubTotal</th>
+          <th scope="col">Descuento</th>
+          <th scope="col">Total</th>
+          <th scope="col">#</th>
         </tr>
       </thead>
       <tbody>
-        @foreach ($cart as $item)
+        @foreach ($cart->items as $item)
           <tr>
             <th scope="row">{{$item->id}}</th>
-            <td>{{$item->name}}</td>
+            <td>{{$item->product->name}}</td>
             <td>{{$item->cant}}</td>
             <td>{{$item->price}}</td>
+            <td>{{$item->sub_total}}</td>
+            <td>{{$item->discount}}</td>
+            <td>{{$item->total}}</td>
+            <td>
+            <form role="form" method="post" name="{{ $item->id }}" id="{{ $item->id }}" action="{{action('CartController@eliminarCarritoItem')}}">
+              <input hidden name="id" value="{{$item->id}}">
+              <button class="btn btn-danger">X</button>
+              {{csrf_field()}}
+            </form>
+
+            </td>
           </tr>
         @endforeach
       </tbody>
     </table>
 
-    <form class="" action="/cartclose" method="post">
-      @csrf
-        <button type="submit" class="btn btn-success">Comprar</button>
-    </form>
+    <h1>Total carrito: {{$cart->total}}</h1>
 
-
+    <form role="form" method="post" action="{{action('CartController@cartclose')}}">
+        <input hidden name="id" value="{{$cart->id}}">
+        <button class="btn btn-success">Comprar</buttonart">
+        {{csrf_field()}}
+      </form>
 </div>
 @endsection
