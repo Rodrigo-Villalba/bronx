4b536cf8ca96bbcab58ec9746d544a332fb77bc8
rodrigo <rodrigogastonvillalba@gmail.com>
2019-11-30 05:18:12 -0300

    saque el drop down

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -38,11 +38,11 @@
             </li>
             @endif
             @endauth
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <li class="nav-item">
+              <a class="nav-link" href="productos">
                 Productos
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              {{-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="productos">Sillas</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="productos">Mesas</a>
@@ -50,21 +50,21 @@
                 <a class="dropdown-item" href="productos">Sillones</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="productos">Deco</a>
-              </div>
+              </div> --}}
             </li>
 
             @auth
             <li class = "nav-item">
               <a href="/history" class="nav-link">  Mis compras</a>
             </li>
-            
+
             <li class="navbar-text">
               <p style="margin-left:250px" class="bienvenido">Bienvenido <span> {{ Auth::user()->name }}</p>
             </li>
             <li style="margin-left:5px" class = "nav-item">
               <a href="/cart" class="nav-link">Carrito</a>
             </li>
-           
+
             <li>
               <a  class="nav-link" href="{{ route('logout') }}"
               onclick="event.preventDefault();
