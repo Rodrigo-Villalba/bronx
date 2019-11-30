84aa26e5838eca9f95b029c11b5ef6eb30f436a3
rodrigo <rodrigogastonvillalba@gmail.com>
2019-11-30 04:15:16 -0300

    agrege primary key

--- app/ImagesProducts.php
+++ app/ImagesProducts.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ImagesProducts extends Model
 {
+  protected  $primaryKey = 'id';
+
   public function product(){
     return $this->belongsTo('App\Product', 'product_id');
   }
--- app/product.php
+++ app/product.php
@@ -6,17 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-  protected $guarded = []; //permitira guardado masivo.
+   protected  $primaryKey = 'id';
+
+   protected $guarded = []; //permitira guardado masivo.
 
  public function user(){
-   return $this->belongsTo('App\User', 'user_id');
+     return $this->belongsTo('App\User', 'user_id');
  }
 
  public function photos(){
      return $this->hasMany('App\ImagesProducts', 'product_id');
-   }
+ }
 
-public function categoria() {
-  return $this->hasOne('App\Categoria', 'categoria_id');
-}
+ public function categoria() {
+     return $this->belongsTo('App\Categoria', 'categoria_id');
+ }
 }
